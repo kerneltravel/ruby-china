a3dff6199275507d540c8db2c88c63adf6fdc35b
kerneltravel <kjpioo2006@gmail.com>
2012-01-09 16:16:31 +0800

    kerneltravel's ruby-china repositry

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,20 @@ set :application, "ruby-china"
 set :repository,  "git://github.com/huacnlee/ruby-china.git"
 set :branch, "master"
 set :scm, :git
-set :user, "ruby"
-set :deploy_to, "/home/#{user}/www/#{application}"
-set :runner, "ruby"
+set :user, "lyl"
+#set :deploy_to, "/home/#{user}/www/#{application}"
+set :deploy_to, "/home/#{user}/oss/#{application}"
+set :runner, "lyl"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 
-role :web, "58.215.172.218"                          # Your HTTP server, Apache/etc
-role :app, "58.215.172.218"                          # This may be the same as your `Web` server
-role :db,  "58.215.172.218", :primary => true # This is where Rails migrations will run
+#role :web, "58.215.172.218"                          # Your HTTP server, Apache/etc
+#role :app, "58.215.172.218"                          # This may be the same as your `Web` server
+#role :db,  "58.215.172.218", :primary => true # This is where Rails migrations will run
+
+role :web, "127.0.0.1"                          # Your HTTP server, Apache/etc
+role :app, "127.0.0.1"                          # This may be the same as your `Web` server
+role :db,  "127.0.0.1", :primary => true # This is where Rails migrations will run
 
 # unicorn.rb 路径
 set :unicorn_path, "#{deploy_to}/current/config/unicorn.rb"
@@ -39,7 +44,9 @@ task :init_shared_path, :roles => :web do
 end
 
 task :link_shared_files, :roles => :web do
-  run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/config.yml #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/mongoid.yml #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/redis.yml #{deploy_to}/current/config/"
   run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
   run "ln -s #{deploy_to}/shared/assets #{deploy_to}/current/public/assets"
 end
@@ -68,8 +75,8 @@ after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared
 
 
 set :default_environment, {
-  'PATH' => "/home/ruby/.rvm/gems/ruby-1.9.3-p0/bin:/home/ruby/.rvm/gems/ruby-1.9.3-p0@global/bin:/home/ruby/.rvm/rubies/ruby-1.9.3-p0/bin:/home/ruby/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
-  'RUBY_VERSION' => 'ruby-1.9.3-p0',
-  'GEM_HOME' => '/home/ruby/.rvm/gems/ruby-1.9.3-p0',
-  'GEM_PATH' => '/home/ruby/.rvm/gems/ruby-1.9.3-p0:/home/ruby/.rvm/gems/ruby-1.9.3-p0@global'
+  'PATH' => "/home/lyl/.rvm/gems/ruby-1.9.2-p290/bin:/home/lyl/.rvm/gems/ruby-1.9.2-p290@global/bin:/home/lyl/.rvm/rubies/ruby-1.9.2-p290/bin:/home/lyl/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
+  'RUBY_VERSION' => 'ruby-1.9.2-p290',
+  'GEM_HOME' => '/home/lyl/.rvm/gems/ruby-1.9.2-p290',
+  'GEM_PATH' => '/home/lyl/.rvm/gems/ruby-1.9.2-p290:/home/lyl/.rvm/gems/ruby-1.9.2-p290@global'
 }
